928675709898d6f09df8df51877e4524aef9fc3e
Boudewijn Bout <boudewijn_bout@outlook.com>
2023-11-11 14:48:52 +0100

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                Console.WriteLine("Cannot divide by zero.");
+                return double.NaN; // NaN (Not a Number) represents an undefined or unrepresentable value.
+            }
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My first calculator");
+
+            Console.WriteLine("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose an operation:");
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2. Subtract");
+            Console.WriteLine("3. Multiply");
+            Console.WriteLine("4. Divide");
+
+            Console.Write("Enter the operation number: ");
+            int operation = Convert.ToInt32(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case 1:
+                    result = Calculator.Add(num1, num2);
+                    break;
+                case 2:
+                    result = Calculator.Subtract(num1, num2);
+                    break;
+                case 3:
+                    result = Calculator.Multiply(num1, num2);
+                    break;
+                case 4:
+                    result = Calculator.Divide(num1, num2);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation.");
+                    break;
+            }
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
